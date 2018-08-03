eda1c2227b916b92df102f1f81db5037a46bc4d1
Desimira <desimira@limechain.tech>
2018-08-03 15:42:58 +0300

    Fixing merge comments

--- restApi/config/settings.js
+++ restApi/config/settings.js
@@ -1,7 +1,9 @@
 const settings = {
     server: {
         port: process.env.NODE_ENV || 8080
-    }
+    },
+    infuraApikey: "XTIF9kIt1kgSOOKclKG0",
+    network:'local'
 }
  
 module.exports = settings;
