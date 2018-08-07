812149b1980ac0c7255c3af26e20d469a6d30acb
Desimira <desimira@limechain.tech>
2018-08-07 16:51:19 +0300

    Small fixes

---
+++ relayer_api/config/templates/keys-templates.js
@@ -0,0 +1,7 @@
+const keys = {
+    deployerPrivateKey: '',
+    libraryAddress: '0x9eD274314f0fB37837346C425D3cF28d89ca9599',
+    infuraApiKey: 'XTIF9kIt1kgSOOKclKG0'
+}
+ 
+module.exports = keys;
